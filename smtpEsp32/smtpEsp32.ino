b7c37ca1cb03140a49a832dc451fdd4e6bd16e00
Markus Felten <Markus.Felten@gmx.de>
2017-09-09 10:01:52 +0200

    feat: deep sleep

--- smtpEsp32/smtpEsp32.ino
+++ smtpEsp32/smtpEsp32.ino
@@ -1,7 +1,11 @@
 #include <WiFi.h>
 #include <time.h>
 #include <esp_deep_sleep.h>
-//#include <soc/rtc.h>
+
+/*
+#include <soc/rtc_cntl_reg.h>
+#include <soc/rtc.h>
+*/
 
 WiFiClient client;
 char formatBuffer[256];
@@ -9,13 +13,16 @@ struct tm timeinfo;
 time_t now;
 
 typedef struct sample {
+    time_t time;
     float battery_voltage;
     float panel_voltage;
 } Sample;
 
-const unsigned int valueSamples = 60;
-Sample values[valueSamples];
+const unsigned int valueSamples = 50;
+RTC_DATA_ATTR Sample values[valueSamples];
 
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  600      /* Time ESP32 will go to sleep (in seconds) */
 
 #include "config.h"
 
@@ -32,6 +39,26 @@ const char* subject      = "";
 const char* smtpUsername = "";
 const char* smtpPassword = "";
 */
+
+
+
+RTC_DATA_ATTR int bootCount = 0;
+
+void print_wakeup_reason(){
+  esp_deep_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_deep_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case 1  : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case 2  : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case 3  : Serial.println("Wakeup caused by timer"); break;
+    case 4  : Serial.println("Wakeup caused by touchpad"); break;
+    case 5  : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.println("Wakeup was not caused by deep sleep"); break;
+  }
+}
 
 void sendString(const char* message) {
   Serial.print(message);
@@ -94,7 +121,7 @@ void sendReport() {
   sendString("\r\n");
 
   for(unsigned int i = 0; i < valueSamples; ++i) {
-    sprintf(formatBuffer, "%f %f\n", values[i].battery_voltage, values[i].panel_voltage);
+    sprintf(formatBuffer, "%d %f %f\n", values[i].time, values[i].battery_voltage, values[i].panel_voltage);
     sendString(formatBuffer);
   }
 
@@ -107,47 +134,67 @@ void sendReport() {
 }
 
 float measure(int pin) {
-  const unsigned int samples = 50;
+  const unsigned int samples = 25;
   float value = 0;
   for(unsigned int i = 0; i < samples; ++i) {
     value += analogRead(pin);
-    delay(10);
+    delay(20);
   }
   return value/(samples*4096)*27.4;
 }
 
 void setup() {
   Serial.begin(115200);
+
+  Serial.println("Boot number: " + String(bootCount));
+  print_wakeup_reason();
+
+  esp_deep_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+  " Seconds");
+ 
   WiFi.begin(wifiSSID, wifiPassword);
   WiFi.setHostname(hostname);
   WiFi.waitForConnectResult();
   configTime(timezone, 0, ndpSever);
 
+  time(&now);
 
-  for(unsigned int i = 0; i < valueSamples; i += 1) {
-    values[i].battery_voltage = measure(34);
-    values[i].panel_voltage = measure(35);
+  localtime_r(&now, &timeinfo);
+  strftime(formatBuffer, sizeof(formatBuffer), "%F %T", &timeinfo);
+  Serial.println(formatBuffer);
+  
+  values[bootCount].time = now;
+  values[bootCount].battery_voltage = measure(34);
+  values[bootCount].panel_voltage = measure(35);
+ 
+  ++bootCount;
+ 
+  if(bootCount % valueSamples == 0) {
+    sendReport();
   }
-  sendReport();
-
+  
+  Serial.println("Going to sleep now");
+  esp_deep_sleep_start();
 
   // TODO: https://github.com/espressif/arduino-esp32/blob/master/tools/sdk/include/soc/soc/rtc.h
   // rtc_init, rtc_sleep_init, rtc_sleep_set_wakeup_time, RTC_TIMER_TRIG_EN
 
 /*
-  struct rtc_sleep_config_t slc = RTC_SLEEP_CONFIG_DEFAULT(RTC_SLEEP_PD_RTC_MEM_FOLLOW_CPU);
+  rtc_sleep_config_t slc = RTC_SLEEP_CONFIG_DEFAULT(RTC_SLEEP_PD_RTC_MEM_FOLLOW_CPU);
   rtc_sleep_init(slc);
 
   uint64_t n = rtc_time_get();
   rtc_sleep_set_wakeup_time(n + 1000000);
   rtc_sleep_start(RTC_TIMER_TRIG_EN, 0);
-  */
+*/
 }
 
 void loop() {
 }
 
 
+/*
 // https://gist.github.com/igrr/54f7fbe0513ac14e1aea3fd7fbecfeab ?
 
 RTC_DATA_ATTR int wake_count;
@@ -159,5 +206,5 @@ void RTC_IRAM_ATTR esp_wake_deep_sleep(void) {
     static RTC_RODATA_ATTR const char fmt_str[] = "Wake count %d\n";
     ets_printf(fmt_str, wake_count++);
 }
-
+*/
 
